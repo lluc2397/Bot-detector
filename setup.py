484cad7aa3140faff9cc6bffc09cdf67c3d60209
Lucas <lluc23@hotmail.com>
2022-10-03 12:38:00 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project to detect if a visiteur is a bot or an actual person',
+    author='Lucas Montes',
+    license='MIT',
+)
---
+++ src/data/process_data.py
@@ -0,0 +1,78 @@
+import pandas as pd
+from pathlib import Path
+
+
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
+DOMAIN = "https://inversionesyfinanzas.xyz"
+DATA_DIR = f"{ROOT_DIR}/data/"
+
+
+def check_correct_path(path: str) -> str:
+    if path.startswith("(") and path.endswith(")"):
+        path = path[1:-2]
+    if not path.startswith(DOMAIN) or path == "/":
+        path = f"{DOMAIN}{path}"
+    return path
+
+
+def arrange_data():
+    visiteurs = pd.read_csv(f"{DATA_DIR}/raw/visiteurs.csv")
+    visiteurs["is_bot"] = 0
+    visiteurs["user_id"] = visiteurs["id"]
+    visiteurs = visiteurs.drop(
+        columns=[
+            "country_name",
+            "continent_code",
+            "continent_name",
+            "session_id",
+            "first_visit_date",
+            "id",
+            "dma_code",
+            "is_in_european_union",
+            "latitude",
+            "longitude",
+            "city",
+            "region",
+            "time_zone",
+            "postal_code",
+        ]
+    )
+    visiteurs["HTTP_USER_AGENT"] = visiteurs["HTTP_USER_AGENT"].fillna("bot")
+    visiteurs.loc[
+        visiteurs["HTTP_USER_AGENT"].str.contains(
+            "|".join(["python", "java", "go", "twitter", "facebook", "requests", "bot"])
+        ),
+        "is_bot",
+    ] = True
+    visiteurs_journey = pd.read_csv(f"{DATA_DIR}/raw/visits_historial_visiteurs.csv")
+    visiteurs_journey = visiteurs_journey.drop(columns=["id", "parsed", "comes_from"])
+    total_seo = pd.merge(visiteurs_journey, visiteurs, on=["user_id"])
+    total_seo = total_seo[total_seo.current_path != "/webmanifest.json"]
+    total_seo.loc[
+        ~total_seo["current_path"].str.contains(
+            "|".join(
+                [
+                    "/publicaciones/",
+                    "/definicion/",
+                    "/screener/analisis-de/",
+                    "/p/",
+                    "/",
+                ]
+            )
+        ),
+        "is_bot",
+    ] = True
+    total_seo["current_path"] = total_seo.apply(
+        lambda row: check_correct_path(row.current_path), axis=1
+    )
+    total_seo["country_code"] = total_seo["country_code"].fillna("Unknown")
+    total_seo = pd.concat(
+        [
+            total_seo,
+            pd.get_dummies(total_seo.country_code.explode()).sum(level=0),
+        ],
+        axis=1,
+        join="inner",
+    )
+    total_seo = total_seo.drop(columns=["user_id", "country_code"])
+    total_seo.to_csv(f"{DATA_DIR}/interim/merged_csv.csv", index=False)
